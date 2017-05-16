06df3d72632c050694504b8730b3ee4fc9379125
Rachel <rachel.f.lew@gmail.com>
2017-05-16 15:12:28 -0400

    Adding landing page with confetti animation

---
+++ confetti.js
@@ -0,0 +1,120 @@
+function startConfetti() {
+  setTimeout(makeItRain, 500, false);
+}
+
+// Based on: https://codepen.io/linrock/pen/Amdhr
+function makeItRain() {
+  var COLORS, Confetti, NUM_CONFETTI, PI_2, canvas, confetti, context, drawCircle, i, range, resizeWindow, xpos;
+
+  NUM_CONFETTI = 350;
+
+  COLORS = [[121, 104, 120], [223, 196, 109], [215, 215, 215], [217, 196, 212], [200, 230, 240]];
+
+  PI_2 = 2 * Math.PI;
+
+  canvas = document.getElementById("confettiCanvas");
+
+  context = canvas.getContext("2d");
+
+  window.w = window.innerWidth;
+  canvas.width = window.innerWidth;
+  window.h = window.innerHeight;
+  canvas.height = window.innerHeight;
+
+  resizeWindow = function() {
+    window.w = canvas.width = window.innerWidth;
+    return window.h = canvas.height = window.innerHeight;
+  };
+
+  window.addEventListener('resize', resizeWindow, false);
+
+  range = function(a, b) {
+    return (b - a) * Math.random() + a;
+  };
+
+  drawCircle = function(x, y, r, style) {
+    context.beginPath();
+    context.arc(x, y, r, 0, PI_2, false);
+    context.fillStyle = style;
+    return context.fill();
+  };
+
+  window.requestAnimationFrame = (function() {
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback) {
+      return window.setTimeout(callback, 1000 / 60);
+    };
+  })();
+
+  xpos = 0.5; 
+
+  Confetti = (function() {
+    function Confetti() {
+      this.style = COLORS[~~range(0, 5)];
+      this.rgb = "rgba(" + this.style[0] + "," + this.style[1] + "," + this.style[2];
+      this.r = ~~range(2, 6);
+      this.r2 = 2 * this.r;
+      this.replace();
+    }
+
+    Confetti.prototype.replace = function() {
+      this.opacity = 0;
+      this.dop = 0.03 * range(1, 4);
+      this.x = range(-this.r2, w - this.r2);
+      this.y = range(-20, h - this.r2);
+      this.xmax = w - this.r;
+      this.ymax = h - this.r;
+      this.vx = range(0, 2) + 8 * xpos - 5;
+      return this.vy = 0.7 * this.r + range(-1, 1);
+    };
+
+    Confetti.prototype.draw = function() {
+      var ref;
+      this.x += this.vx;
+      this.y += this.vy;
+      this.opacity += this.dop;
+      if (this.opacity > 1) {
+        this.opacity = 1;
+        this.dop *= -1;
+      }
+      if (this.opacity < 0 || this.y > this.ymax) {
+        this.replace();
+      }
+      if (!((0 < (ref = this.x) && ref < this.xmax))) {
+        this.x = (this.x + this.xmax) % this.xmax;
+      }
+      return drawCircle(~~this.x, ~~this.y, this.r, this.rgb + "," + this.opacity + ")");
+    };
+
+    return Confetti;
+
+  })();
+
+  confetti = (function() {
+    var j, ref, results;
+    results = [];
+    for (i = j = 1, ref = NUM_CONFETTI; 1 <= ref ? j <= ref : j >= ref; i = 1 <= ref ? ++j : --j) {
+      results.push(new Confetti);
+    }
+    return results;
+  })();
+
+  window.step = function() {
+    var c, j, len, results;
+    requestAnimationFrame(step);
+    context.clearRect(0, 0, w, h);
+    results = [];
+    for (j = 0, len = confetti.length; j < len; j++) {
+      c = confetti[j];
+      results.push(c.draw());
+    }
+    return results;
+  };
+
+  step();
+
+}
+
+function stopConfetti() {
+  var canvas = document.getElementById("confettiCanvas");
+  canvas.classList.add("hidden");
+}
--- sheprolewido.js
+++ sheprolewido.js
@@ -1,12 +1,22 @@
 function selectMenuItem(elem) {
+  stopConfetti();
+
+  // Unselect any selected menu items
   var selectedItems = document.getElementsByClassName("menuItem selected");
   for (i = 0; i < selectedItems.length; i++) {
     var element = selectedItems[i];
     element.classList.remove("selected");
-    var sectId = element.id + "Sect";
-    var section = document.getElementById(sectId);
-    section.classList.add("hidden");
   }
+  // Hide any currently visible sections
+  var visibleSections = document.getElementsByClassName("centerContent");
+  for (i = 0; i < visibleSections.length; i++) {
+    var element = visibleSections[i];
+    if (element.classList.contains("hidden") != true) {
+      element.classList.add("hidden");
+    }
+  }
+
+  // Select current menu item and reveal it's section
   elem.classList.add("selected");
   var selectedSectId = elem.id + "Sect";
   var selectedSection = document.getElementById(selectedSectId);
